4a83963ef393e7e2dbff94853892e6c45d81d726
Brian Olsen <brian@maven-group.org>
2019-05-02 22:43:47 +0200

    Add more example devices and click on flag to change

--- features.js
+++ features.js
@@ -22,16 +22,43 @@ function setBitClassName(prefix, value, selected, notSelected) {
     }
   }
 }
-function featuresExample(el) {
-  document.getElementById("airplay-features-input").value = el.value;
-  updateFeatures(el.value);
-}
-function featuresChanged(el) {
-  document.getElementById("airplay-features-examples").value = "";
-  updateFeatures(el.value);
+function updateFlagTable(prefix, value) {
+  if (value.equals(Long.ZERO)) {
+    document.getElementById(prefix + "-output").className = "noscript";
+    setBitClassName(prefix, Long.ZERO, "", "")
+    document.getElementById(prefix).className = "";
+  } else {
+    document.getElementById(prefix + "-output").className = "";
+    setBitClassName(prefix, value, "selected", "notSelected")
+    document.getElementById(prefix).className = "selecting";
+  }
+
+  document.getElementById(prefix + "-dec").innerHTML = value.toString(10);
+  document.getElementById(prefix + "-hex").innerHTML = "0x" + value.toString(16);
 }
 
-function updateFeatures(featuresS) {
+function exampleChanged(prefix, fn) {
+  var el = document.getElementById(prefix + "-examples");
+  document.getElementById(prefix + "-input").value = el.value;
+  fn(el);
+}
+
+function inputChanged(prefix, fn, bit) {
+  var el = document.getElementById(prefix + "-input");
+  document.getElementById(prefix + "-examples").value = "";
+  document.getElementById(prefix + "-interactive").className = "";
+  fn(el, bit);
+}
+
+function flipBit(prefix, fn, bit) {
+  var el = document.getElementById(prefix + "-input");
+  document.getElementById(prefix + "-examples").value = "";
+  document.getElementById(prefix + "-interactive").className = "";
+  fn(el, bit)
+}
+
+function updateFeatures(el, bit) {
+  var featuresS = el.value;
   var comma = featuresS.indexOf(",");
   var features = 0;
   if (comma >= 0) {
@@ -41,46 +68,55 @@ function updateFeatures(featuresS) {
   } else {
     features = toLong(featuresS);
   }
-  if (features.equals(Long.ZERO)) {
-    document.getElementById("airplay-features-output").className = "noscript";
-    setBitClassName("airplay-features", Long.ZERO, "", "")
-  } else {
-    document.getElementById("airplay-features-output").className = "";
-    setBitClassName("airplay-features", features, "selected", "notSelected")
+  if (typeof bit != "undefined") {
+    var b = Long.ONE.shiftLeft(bit);
+    features = features.xor(b);
+    el.value = "0x" + features.toString(16);
   }
-
-  document.getElementById("airplay-features-dec").innerHTML = features.toString(10);
-  document.getElementById("airplay-features-hex").innerHTML = "0x" + features.toString(16);
+  updateFlagTable("airplay-features", features);
   var txt = "0x" + features.and(Long.fromBits(0xffffffff, 0, true)).toString(16) +
     ",0x" + features.shiftRight(32).toString(16);
   document.getElementById("airplay-features-txt").innerHTML = txt;
 }
 
-function systemFlagsExample(el) {
-  document.getElementById("airplay-system-flags-input").value = el.value;
-  updateSystemFlags(el.value);
-}
-function systemFlagsChanged(el) {
-  document.getElementById("airplay-system-flags-examples").value = "";
-  updateSystemFlags(el.value);
-}
-
-function updateSystemFlags(systemFlagsS) {
+function updateSystemFlags(el, bit) {
+  var systemFlagsS = el.value;
   var systemFlags = 0;
   systemFlags = toLong(systemFlagsS);
-  if (systemFlags.equals(Long.ZERO)) {
-    document.getElementById("airplay-system-flags-output").className = "noscript";
-    setBitClassName("airplay-system-flags", Long.ZERO, "", "")
-  } else {
-    document.getElementById("airplay-system-flags-output").className = "";
-    setBitClassName("airplay-system-flags", systemFlags, "selected", "notSelected")
+  if (typeof bit != "undefined") {
+    var b = Long.ONE.shiftLeft(bit);
+    systemFlags = systemFlags.xor(b);
+    el.value = "0x" + systemFlags.toString(16);
   }
-
-  document.getElementById("airplay-system-flags-dec").innerHTML = systemFlags.toString(10);
-  document.getElementById("airplay-system-flags-hex").innerHTML = "0x" + systemFlags.toString(16);
+  updateFlagTable("airplay-system-flags", systemFlags);
 }
 
-featuresChanged(document.getElementById("airplay-features-input"));
-systemFlagsChanged(document.getElementById("airplay-system-flags-input"));
-document.getElementById("airplay-features-interactive").className = "";
-document.getElementById("airplay-system-flags-interactive").className = "";
+function installHandlers(prefix, fn) {
+  var input = function() {
+    inputChanged(prefix, fn);
+  }
+  var example = function() {
+    exampleChanged(prefix, fn);
+  }
+  var changeBit = function(ev) {
+    var target = ev.target;
+    while (target != null && target.tagName != "TR") {
+      target = target.parentElement;
+    }
+    if (target == null) {
+      return;
+    }
+    var bit = parseInt(target.id.substring(prefix.length + "-bit".length));
+    inputChanged(prefix, fn, bit);
+  }
+  var el = document.getElementById(prefix + "-input");
+  el.onkeyup = input;
+  el.onpaste = input;
+  var el = document.getElementById(prefix + "-examples");
+  el.onchange = example;
+  var el = document.getElementById(prefix + "-bit0");
+  el.parentNode.onclick = changeBit;
+  input();
+}
+installHandlers("airplay-features", updateFeatures)
+installHandlers("airplay-system-flags", updateSystemFlags)
