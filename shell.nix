7cf4171576299d97bc0b3e9f4c247075d79af1c7
Brian Olsen <brian@maven-group.org>
2020-06-15 21:59:45 +0200

    Lock down nixpkgs with niv

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
 let
-  pkgs = import <nixpkgs> {};
+  sources = import ./nix/sources.nix;
+  nixpkgs = import sources.nixpkgs {
+    overlays = [ (self: super: { niv = (import sources.niv {}).niv; }) ];
+  };
 in
-  with pkgs;
-  pkgs.stdenv.mkDerivation {
+  with nixpkgs;
+  stdenv.mkDerivation {
     name = "airplay-spec";
     src = ./.;
-    buildInputs = [mdbook boot jdk12];
-}
+    buildInputs = [
+      mdbook boot jdk12 niv
+    ];
+  }
